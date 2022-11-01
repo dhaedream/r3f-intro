7702967c08e3e0ec940e754e23bf9d5a321e3bc3
Dhaesha M <dhaesha1@gmail.com>
2022-10-31 22:21:18 -0400

    scene set up

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,9 +3,17 @@ import React from "react";
 const Experience = () => {
   return (
     <>
-      <mesh>
-        <sphereGeometry args={[1.5, 32, 32]} />
+      <mesh position-x={-1}>
+        <sphereGeometry />
+        <meshBasicMaterial color="orange" />
+      </mesh>
+      <mesh position={[1, 0, 0]}>
+        <sphereGeometry />
         <meshBasicMaterial color="purple" wireframe />
+      </mesh>
+      <mesh position-y={-2} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshBasicMaterial color="green" />
       </mesh>
     </>
   );
