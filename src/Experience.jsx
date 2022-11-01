640b788223d22369aa9bf2eb8b5272acd716727a
Dhaesha M <dhaesha1@gmail.com>
2022-10-31 22:03:59 -0400

    add wireframe to mesh

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,8 +4,8 @@ const Experience = () => {
   return (
     <>
       <mesh>
-        <sphereGeometry />
-        <meshBasicMaterial />
+        <sphereGeometry args={[1.5, 32, 32]} />
+        <meshBasicMaterial color="purple" wireframe />
       </mesh>
     </>
   );
