72602a3101430aa676b060ae1e3b2d2b47078b76
Dhaesha M <dhaesha1@gmail.com>
2022-10-31 21:58:27 -0400

    experimenting w meshes

---
+++ src/Experience.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <>
+      <mesh>
+        <sphereGeometry />
+        <meshBasicMaterial />
+      </mesh>
+    </>
+  );
+};
+
+export default Experience;
