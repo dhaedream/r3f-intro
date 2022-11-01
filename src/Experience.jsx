36e95d27b27991e36e9921c4dce6b259cca22027
Dhaesha M <dhaesha1@gmail.com>
2022-10-31 22:31:41 -0400

    animate mesh to rotate on y axis at slow speed

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 const Experience = () => {
+  const cubeRef = useRef();
+  useFrame(() => {
+    cubeRef.current.rotation.y += 0.003;
+  });
+
   return (
     <>
       <mesh position-x={-1}>
         <sphereGeometry />
         <meshBasicMaterial color="orange" />
       </mesh>
-      <mesh position={[1, 0, 0]}>
+      <mesh ref={cubeRef} position={[1, 0, 0]}>
         <sphereGeometry />
         <meshBasicMaterial color="purple" wireframe />
       </mesh>
-      <mesh position-y={-2} rotation-x={-Math.PI * 0.5} scale={10}>
+      <mesh position-y={-2} rotation-x={-Math.PI * 0.5} scale={15}>
         <planeGeometry />
         <meshBasicMaterial color="green" />
       </mesh>
